b56f0b6b022b439257c6a4ec41377fa358c98735
grohoprecision-hub <grohoprecision@gmail.com>
2026-07-03 15:45:38 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
-
+output: 'static',
 export default defineConfig({
   site: 'https://kravzik.com',
 
